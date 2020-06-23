c34ea3253b0173788884f44dadc2cc76a0cd63f6
Mark <m.hern1415@gmail.com>
2020-06-23 15:12:06 -0700

    added src folders

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './Header';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    )
+}
+
+export default App;
