4553aceb6d048029a9bff346f0c5d6a6d7423308
Mark <m.hern1415@gmail.com>
2020-06-25 16:54:19 -0700

    added reducer and action to push data to api

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,4 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from './types';
+import contacts from '../apis/contacts';
+import axios from 'axios'
 
 export const signIn = (userId) => {
     return {
@@ -12,3 +14,18 @@ export const signOut = () => {
         type: SIGN_OUT
     };
 };
+
+export const addContact = (contact, history) => {
+    return dispatch => {
+      return fetch('http://localhost:3002/contacts', {
+        method: "POST",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({contact})
+      }).then(resp => resp.json())
+        .then(contact => {
+            dispatch({type: "ADD_CONTACT", contact})
+            history.push('/contacts')
+        }
+      )
+    }
+  }
